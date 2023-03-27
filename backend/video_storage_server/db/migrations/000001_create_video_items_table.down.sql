7006309ba41037c949db63fcadb9393fc6e932b0
yud-warrior <rodionyudenko@gmail.com>
2023-03-27 15:01:33 +0300

    db and migrations were added

---
+++ backend/video_storage_server/db/migrations/000001_create_video_items_table.down.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS video_items(
+pk UUID NOT NULL DEFAULT gen_randon_uuid(),
+/*id SERIAL PRIMARY KEY,*/
+name VARCHAR(100) NOT NULL UNIQUE,
+short_description VARCHAR(256),
+full_description TEXT,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+safe_version BOOLEAN NOT NULL DEFAULT false,
+CONSTRAINT pk_video_items PRIMARY KEY (pk),
+);
