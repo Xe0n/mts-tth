5d2abc58b82dfc640cec6212e57bab4f3a37f161
yud-warrior <rodionyudenko@gmail.com>
2023-03-27 23:19:47 +0300

    bash scripts for migrate up and down

--- backend/video_storage_server/db/migrations/000001_create_video_items_table.up.sql
+++ backend/video_storage_server/db/migrations/000001_create_video_items_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS video_items(
-pk UUID NOT NULL DEFAULT gen_randon_uuid(),
+pk UUID NOT NULL DEFAULT gen_random_uuid(),
 name VARCHAR(100) NOT NULL UNIQUE,
 short_description VARCHAR(256),
 full_description TEXT,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 safe_version BOOLEAN NOT NULL DEFAULT false,
-CONSTRAINT pk_video_items PRIMARY KEY (pk),
+CONSTRAINT pk_video_items PRIMARY KEY (pk)
 );
