fd64476c4583b0498c7eb63012d5f8cadeaa58c1
yud-warrior <rodionyudenko@gmail.com>
2023-03-28 15:23:33 +0300

    db auth from ENV added to main.go

--- backend/video_storage_server/main.go
+++ backend/video_storage_server/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/yud-warrior/video-storage-server/controllers"
+	"github.com/yud-warrior/video-storage-server/db"
 	"github.com/yud-warrior/video-storage-server/middlewares"
 )
 
 func main() {
+	dbUser, dbPassword, dbName :=
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB")
+	database, err := db.Initialize(dbUser, dbPassword, dbName)
+	if err != nil {
+		log.Fatalf("Could not set up database: %v", err)
+	}
+	defer database.Conn.Close()
+
 	router := gin.Default()
 	r := router.Group("/api/v1")
 	r.Use(middlewares.CORSMiddleware())
